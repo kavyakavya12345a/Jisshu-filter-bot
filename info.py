3a1e79ecca0fa31f965c8344fddcfb664e8a146f
kavyakavya12345a <opkavyatele@gmail.com>
2025-04-04 08:16:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ API_HASH = environ.get('API_HASH', 'adbbccf8eed67602e3c13f2524272ae6')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5694158462').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/eddard_starkES") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002298463307'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/happybhirfhsjak')
