09894d457df297af7ed95ea572d532052b074567
kavyakavya12345a <opkavyatele@gmail.com>
2025-04-04 21:08:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5694158462').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/eddard_starkES") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002298463307'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/happybhirfhsjak')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+XEjkzYbu0iA5MTJl')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002450286141 -1002277566217').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
